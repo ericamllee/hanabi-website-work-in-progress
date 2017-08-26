a556754d816ffce99f24352309acdea0d27a84ab
Erica Lee <ericamllee@gmail.com>
2017-08-25 23:54:33 -0700

    everything

--- playGame/models.py
+++ playGame/models.py
@@ -3,5 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Game(models.Model):
+  name = models.CharField(max_length=200)
+
 class Person(models.Model):
   name = models.CharField(max_length=200)
+  game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
+
--- playGame/views.py
+++ playGame/views.py
@@ -5,27 +5,34 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from playGame.models import Person
+from playGame.models import Person, Game
 
-# todo: put this on the database
-person_id = 5
-names = [Person(name="Benny", id=1), Person(name="Tenny", id=2), Person(name="Renny", id=3), Person(name="Henny", id=4)]
 
 def index(request):
-  global person_id
   if request.method == "GET":
     return render(request, 'playGame/index.html')
   elif request.method == "POST":
     if 'playername' in request.POST and request.POST['playername']:
-      names.append(Person(name=request.POST['playername'], id = person_id))
-      person_id += 1
+      p = Person(name=request.POST['playername'])
+      p.save()
       return HttpResponseRedirect(reverse('playGame:join'))
     else:
-      # todo: add name into names
       return render(request, 'playGame/index.html', {'error_message': "You need to fill in a name"})
 
+
 def play(request):
-  return render(request, 'playGame/game.html')
+  return render(request, 'playGame/play.html')
 
 def join(request):
-  return render(request, 'playGame/join.html', {'names': names})
+  # todo: filter names for people not in games.
+  if request.method == "GET":
+    people = Person.objects.all()
+    return render(request, 'playGame/join.html', {'people': people})
+  elif request.method == "POST":
+    if True:
+      # todo: only allow 2-4 players in a game.
+      # todo: have a way to accept game requests from people.
+      return HttpResponseRedirect(reverse('playGame:play'))
+    else:
+      return render(request, 'playGame/join.html', {'error_message': "You must have between 2-4 players"})
+
