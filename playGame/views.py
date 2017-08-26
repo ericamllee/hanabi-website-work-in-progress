4102550b95e89c57b3910612a3767785a83f2ac7
Erica Lee <ericamllee@gmail.com>
2017-08-25 23:19:22 -0700

    adding names to game list

--- playGame/views.py
+++ playGame/views.py
@@ -7,13 +7,18 @@ from django.template import loader
 from django.urls import reverse
 from playGame.models import Person
 
+# todo: put this on the database
+person_id = 5
 names = [Person(name="Benny", id=1), Person(name="Tenny", id=2), Person(name="Renny", id=3), Person(name="Henny", id=4)]
 
 def index(request):
+  global person_id
   if request.method == "GET":
     return render(request, 'playGame/index.html')
   elif request.method == "POST":
     if 'playername' in request.POST and request.POST['playername']:
+      names.append(Person(name=request.POST['playername'], id = person_id))
+      person_id += 1
       return HttpResponseRedirect(reverse('playGame:join'))
     else:
       # todo: add name into names
